971b6b33c333bee4e4d2b6434b0fcc304e336349
anaMariaJimenezPerez <90760700+anaMJimenezPerez@users.noreply.github.com>
2023-03-25 23:34:13 +0000

    Integrado el back con el front completamente

--- gasolineras.js
+++ gasolineras.js
@@ -22,15 +22,7 @@ export async function buscador_gasolineras(radio, coords) {
 }
 
  
-export function presentadorGasolineras(markers,gasolineras,map) {
-    var iconGas = new L.icon({
-        iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-orange.png',
-        shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
-        iconSize: [25, 41],
-        iconAnchor: [12, 41],
-        popupAnchor: [1, -34],
-        shadowSize: [41, 41]
-    })
+export function presentadorGasolineras(markers,gasolineras,map, iconGas) {
     gasolineras.forEach(element => {
         let coordGasolinera = element.split(",");
         console.log(coordGasolinera);
