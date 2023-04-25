3d07bda3e33fc13f48d99ebf370120cefd34449d
David Sosa Santana <113687894+DavidDenoia@users.noreply.github.com>
2023-04-25 18:07:11 +0100

    Menu usuario desplegado basico sin CSS
    Buena suerte Marco

--- scripts/script.js
+++ scripts/script.js
@@ -154,4 +154,13 @@ document.getElementById('customRange3').addEventListener('change', function() {
 });
 
 
-
+document.getElementById("icono-usuario").addEventListener("click", function () {
+  //document.getElementById("botones-container").style.display = "none";
+  //document.getElementById("slider-container").style.display = "none";
+  var dropdownMenu = document.getElementById("usuario-desplegable");
+  if (dropdownMenu.style.display === "none") {
+    dropdownMenu.style.display = "block";
+  } else {
+    dropdownMenu.style.display = "none";
+  }
+});
