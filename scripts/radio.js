df240c990ac51d58adbb8fd4f6efbe019efca960
anaMariaJimenezPerez <90760700+anaMJimenezPerez@users.noreply.github.com>
2023-05-01 22:29:30 +0100

    Revert "se muestra la lista de gasolineras al hacer busqueda con radio"
    This reverts commit 44a43efd4929e37e8d34b80d3d184efc9b7212f8.

--- scripts/radio.js
+++ scripts/radio.js
@@ -1,10 +1,6 @@
-import {
-  buscador_gasolineras,
-  buscadorInformacionGasolinera,
-  pushMarcadorInformacion,
-} from "./gasolineras.js";
-import { sortGasolineras } from "./listas.js";
+import { buscador_gasolineras, buscadorInformacionGasolinera, pushMarcadorInformacion} from "./gasolineras.js";
 import { baseLayer, borrarMierdaDelMapa } from "./script.js";
+
 
 export async function mostrarRatio(map, markers, iconGas) {
   //Variables necesarias para mostrar las gasolineras dentro de un ratio
@@ -12,9 +8,9 @@ export async function mostrarRatio(map, markers, iconGas) {
   var marks = L.layerGroup().addTo(map);
   var listaGasolineras = [];
   console.log("Entro a mostrarRaio");
-  var radio = Number(localStorage.getItem("radio"));
-
-  if (isNaN(radio)) {
+  var radio = Number(localStorage.getItem('radio'));
+  
+  if(isNaN(radio)) {
     radio = 2800;
   }
   console.log(radio);
@@ -26,9 +22,9 @@ export async function mostrarRatio(map, markers, iconGas) {
   //    if (e.keyCode === 13) {
   //      e.preventDefault(); //Prevenir el envio del formulario
 
-  borrarMierdaDelMapa(markers, baseLayer);
+        borrarMierdaDelMapa(markers,baseLayer);
 
-  /*for (let j = 0; j < markers.length; j++) {
+        /*for (let j = 0; j < markers.length; j++) {
           map.removeLayer(markers[j]);
         };
         map.eachLayer((layer) => {
@@ -36,72 +32,65 @@ export async function mostrarRatio(map, markers, iconGas) {
             map.removeLayer(layer);
           }
         });*/
+        
 
-  var ubicacion = document.getElementById("ubicacion").value;
+        var ubicacion = document.getElementById("ubicacion").value;
 
-  //Obtener las coordenadas de la ubicación utilizando la API de OpenStreetMap
-  var ubicacion_url =
-    "https://nominatim.openstreetmap.org/search.php?q=" +
-    ubicacion +
-    "&format=jsonv2";
-  fetch(ubicacion_url)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      console.log(data);
-      var ubicacionCoords = [parseFloat(data[0].lat), parseFloat(data[0].lon)]; //Coordenadas de la ubicación
+        //Obtener las coordenadas de la ubicación utilizando la API de OpenStreetMap
+        var ubicacion_url =
+          "https://nominatim.openstreetmap.org/search.php?q=" +
+          ubicacion +
+          "&format=jsonv2";
+        fetch(ubicacion_url)
+          .then(function (response) {
+            return response.json();
+          })
+          .then(function (data) {
+            console.log(data);
+            var ubicacionCoords = [
+              parseFloat(data[0].lat),
+              parseFloat(data[0].lon),
+            ]; //Coordenadas de la ubicación
 
-      console.log(ubicacionCoords);
+            console.log(ubicacionCoords);
 
-      //ELiminar marcadores
-      marks.clearLayers();
+            //ELiminar marcadores
+            marks.clearLayers();
 
-      // Actualizar el círculo en el mapa
-      if (circle) {
-        map.removeLayer(circle);
-      }
+            // Actualizar el círculo en el mapa
+            if (circle) {
+              map.removeLayer(circle);
+            }
 
-      circle = L.circle(ubicacionCoords, {
-        radius: radio, // radio en metros
-        color: "blue", // color del borde
-        fillColor: "#3388ff", // color de relleno
-        fillOpacity: 0.2, // opacidad del relleno
-        interactive: false,
-      }).addTo(map);
 
-      //El mapa se ajusta a la busqueda realizada
-      map.flyToBounds(circle.getBounds(), { duration: 1 });
+            circle = L.circle(ubicacionCoords, {
+              radius: radio, // radio en metros
+              color: "blue", // color del borde
+              fillColor: "#3388ff", // color de relleno
+              fillOpacity: 0.2, // opacidad del relleno
+              interactive: false
+            }).addTo(map);
 
-      // Llamar a la función para mostrar las gasolineras dentro del radio
-      buscador_gasolineras(radio, ubicacionCoords).then(function (result) {
-        console.log("Longitud: " + ubicacionCoords[0]);
-        buscadorInformacionGasolinera(result).then((info) => {
-          console.log(info);
-          listaGasolineras = [];
-          info.forEach((gasolinera) => {
-            console.log(gasolinera);
-            pushMarcadorInformacion(markers, gasolinera, map, iconGas, listaGasolineras);
+            //El mapa se ajusta a la busqueda realizada 
+            map.flyToBounds(circle.getBounds(), {duration: 1});
+
+            // Llamar a la función para mostrar las gasolineras dentro del radio
+            buscador_gasolineras(radio, ubicacionCoords).then(function (result) {
+              console.log("Longitud: " + ubicacionCoords[0]);
+              buscadorInformacionGasolinera(result).then((info) => {
+                console.log(info);
+                listaGasolineras = [];
+                info.forEach((gasolinera) => {
+                  console.log(gasolinera);
+                  pushMarcadorInformacion( markers, gasolinera, map, iconGas, listaGasolineras);
+                });
+              });
+            });
           });
-        });
-      });
-    });
-  //mostrar lista de gasolineras
-  var switchListaGasolineras = false;
-  document
-    .getElementById("gasolineraLista")
-    .addEventListener("click", function () {
-      if (!switchListaGasolineras) {
-        sortGasolineras(listaGasolineras);
-        switchListaGasolineras = true;
-      } else {
-        clearListaGasolineras();
-        switchListaGasolineras = false;
       }
-    });
-}
 //    });
 //}
+
 
 /*export async function mostrarRatioActualizado(map, markers, iconGas, radio) {
 
@@ -119,7 +108,7 @@ export async function mostrarRatio(map, markers, iconGas) {
     }
   });*/
 
-/*var ubicacion = document.getElementById("ubicacion").value;
+  /*var ubicacion = document.getElementById("ubicacion").value;
 
         //Obtener las coordenadas de la ubicación utilizando la API de OpenStreetMap
         var ubicacion_url =
