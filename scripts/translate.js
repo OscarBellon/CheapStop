48375fd5dbbe1100c320a8ac81126e80b0958ee2
Oscar <oscar.bellon101@alu.ulpgc.es>
2023-05-12 11:20:44 +0100

    avances en traduccion de trajetas

--- scripts/translate.js
+++ scripts/translate.js
@@ -12,6 +12,7 @@ function changeLanguaje(idioma){
     language = idioma;
     translate(idioma);
     translatePlaceholder(idioma);
+    translateCards(idioma);
 }
 
 function translate(idioma){ 
@@ -81,6 +82,40 @@ function updateWeb(docs){
         document.getElementById(id).textContent = docs[id];
     }
 
+}
+
+export function translateCards(idioma){
+
+    console.log("Traduciendo...")
+
+    switch(idioma){
+
+        case "es":
+            fetch("/translations/es_placeholder.json").then(result => result.json()).then(res => {updateNodes(res)});
+            break
+
+        case "en":
+            fetch("/translations/en_placeholder.json").then(result => result.json()).then(res => {updateNodes(res)});
+            break
+
+        case "eo":
+            console.log("Esperanto")
+            fetch("/translations/eo_placeholder.json").then(result => result.json()).then(res => {updateNodes(res)});
+            break
+
+    }
+
+}
+
+function updateNodes(docs){
+    let ids = Object.keys(docs);
+    for(let i=0; i<ids.length;i++){
+        let id = ids[i];
+        console.log(id)
+        document.querySelectorAll("id").forEach(elemento => {
+            elemento.textContent=docs[id];
+        })
+    }
 }
 
 document.getElementById("idiomaeo").addEventListener("click", () => {changeLanguaje("eo")});
